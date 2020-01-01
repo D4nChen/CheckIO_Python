9b8c4bfbc81d5c1d13406898075982bf56b5c852
Mr13Dan <DanilaChernenko@gmail.com>
2020-01-01 22:02:28 +0200

    Create Multiply(Intro).py

---
+++ Elementary/Multiply(Intro).py
@@ -0,0 +1,10 @@
+def mult_two(a, b):
+    c = a * b
+    return c
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(mult_two(3, 2))
+    assert mult_two(1, 0) == 0
+    print("Coding complete? Click 'Check' to earn cool rewards!")
